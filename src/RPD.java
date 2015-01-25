343dd462702b8de369b75615227a25eaf8f5d92c
matteocam <matteo.campanelli@gmail.com>
2015-01-25 00:41:57 -0500

    Added stub RPD

---
+++ src/RPD.java
@@ -0,0 +1,16 @@
+
+// Repeated Prisoners Dilemma
+public class RPD
+{
+
+}
+
+static class PD 
+{
+    public static final gameMatrix[2][2]
+    
+    public static void play(PrisonersDilemmaStrategy player1, PrisonersDilemmaStrategy player2)
+    {
+	// players should be notified about the other player's move and rcord the history somehow
+    }
+}
